e376a89db85a9a6f246309325f2818aaa413e236
Bogdan Chadkin <trysound@yandex.ru>
2018-05-21 01:11:18 +0300

    Add umd bundle for browser use and tracking whole library size (#72)
    * Add umd bundle for browser use and tracking whole library size
    
    * rebase

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,7 @@
+import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import replace from 'rollup-plugin-replace';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 import pkg from './package.json';
 
@@ -6,18 +9,47 @@ const input = './src/index.js';
 
 const external = id => !id.startsWith('.') && !id.startsWith('/');
 
+const globals = {
+  react: 'React',
+  'prop-types': 'PropTypes',
+};
+
+const getBabelOptions = () => ({
+  exclude: '**/node_modules/**',
+  runtimeHelpers: true,
+});
+
 export default [
+  {
+    input,
+    output: {
+      file: 'dist/react-event-listener.umd.js',
+      format: 'umd',
+      exports: 'named',
+      name: 'ReactEventListener',
+      globals,
+    },
+    external: Object.keys(globals),
+    plugins: [
+      nodeResolve(),
+      babel(getBabelOptions()),
+      commonjs({ include: '**/node_modules/**' }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+      sizeSnapshot(),
+    ],
+  },
+
   {
     input,
     output: { file: pkg.main, format: 'cjs', exports: 'named' },
     external,
-    plugins: [babel({ runtimeHelpers: true })],
+    plugins: [babel(getBabelOptions())],
   },
 
   {
     input,
     output: { file: pkg.module, format: 'es', exports: 'named' },
     external,
-    plugins: [babel({ runtimeHelpers: true }), sizeSnapshot()],
+    plugins: [babel(getBabelOptions()), sizeSnapshot()],
   },
 ];
